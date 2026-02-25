51229ffd8464c5a814b52d2c6d7061736c371e39
aman-sharma14 <sharma.aman.14@proton.me>
2026-02-25 22:18:13 +0530

    cors fixed

--- backend/api-gateway/src/main/java/com/streamix/api_gateway/GlobalCorsConfiguration.java
+++ backend/api-gateway/src/main/java/com/streamix/api_gateway/GlobalCorsConfiguration.java
@@ -1,12 +1,12 @@
 package com.streamix.api_gateway;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 public class GlobalCorsConfiguration {
@@ -14,7 +14,7 @@ public class GlobalCorsConfiguration {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://streamix-live.vercel.app"));
         corsConfig.setMaxAge(3600L);
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
